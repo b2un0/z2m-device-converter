22eb0d6670fa9d340a155a049e04a7cab4c8ed6a
Branko Wilhelm <branko.wilhelm@gmail.com>
2023-09-28 17:02:23 +0200

    vcs import

---
+++ README.md
@@ -0,0 +1,5 @@
+# zigbee2mqtt custom converters
+
+add the file for your device from the [converter](converter/) Folder as `external converter` as described in the official documentation
+
+https://www.zigbee2mqtt.io/advanced/support-new-devices/01_support_new_devices.html
---
+++ converter/_TZE200_mby4kbtq.js
@@ -0,0 +1,39 @@
+const exposes = require('zigbee-herdsman-converters/lib/exposes');
+const e = exposes.presets;
+const ea = exposes.access;
+const tuya = require('zigbee-herdsman-converters/lib/tuya');
+
+const definition = {
+    fingerprint: [
+        {
+            modelID: 'TS0601',
+            manufacturerName: '_TZE200_mby4kbtq'
+        }
+    ],
+    model: 'TS0601_gas_sensor_3',
+    vendor: 'TuYa',
+    description: 'Gas sensor',
+    fromZigbee: [tuya.fz.datapoints],
+    toZigbee: [tuya.tz.datapoints],
+    configure: tuya.configureMagicPacket,
+    exposes: [
+        e.gas(),
+        tuya.exposes.gasValue().withUnit('LEL'),
+        exposes.binary('preheat', ea.STATE, true, false),
+        tuya.exposes.faultAlarm(),
+        exposes.binary('alarm_switch', ea.STATE_SET, true, false),
+        tuya.exposes.silence()
+    ],
+    meta: {
+        tuyaDatapoints: [
+            [1, 'gas', tuya.valueConverter.trueFalse0],
+            [2, 'gas_value', tuya.valueConverter.divideBy10],
+            [10, 'preheat', tuya.valueConverter.raw],
+            [11, 'fault_alarm', tuya.valueConverter.trueFalse1],
+            [13, 'alarm_switch', tuya.valueConverter.raw],
+            [16, 'silence', tuya.valueConverter.raw],
+        ],
+    },
+};
+
+module.exports = definition;
